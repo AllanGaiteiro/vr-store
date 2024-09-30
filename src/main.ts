05914f1efcd92a4a083d61a57c6635a2677a3a32
allangaiteiro <allansgaiteiro@gmail.com>
2024-09-30 14:42:33 -0300

    feat: set config heroku port

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document); // Define a rota da documentação
-
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
