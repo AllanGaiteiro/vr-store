c44258769ccb4099e30aedc27559d72b19e4fd29
allangaiteiro <allansgaiteiro@gmail.com>
2024-09-30 14:14:28 -0300

    feat; create route api-docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuração do Swagger
+  const config = new DocumentBuilder()
+    .setTitle('VR Store API')  // Título da documentação
+    .setDescription('API para gerenciamento de produtos, lojas e preços') // Descrição da API
+    .setVersion('1.0') // Versão da API
+    .addTag('products') // Exemplo de tag para organizar endpoints
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document); // Define a rota da documentação
+
   await app.listen(3000);
 }
 bootstrap();
